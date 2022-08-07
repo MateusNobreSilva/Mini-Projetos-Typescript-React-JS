2411bbea8dbb48cd4d7f65fbddb22faca23f7db4
MateusNobreSilva <mateusnobresilva@gmail.com>
2022-08-07 19:07:21 -0300

    retirando comentarios

--- src/Pages/BackgroundColor/styled.ts
+++ src/Pages/BackgroundColor/styled.ts
@@ -7,18 +7,13 @@ interface ContainerProps {
 export const Container = styled.div<ContainerProps>`
   display: flex;
   // flex-direction: row | row-reverse | column | column-reverse;
-  flex-direction: row;
+  flex-direction: column;
   flex-wrap: wrap;
+  align-content: center;
   justify-content: space-around;
   align-items: center;
-  align-content: flex-start;
+
   width: 100%;
-  height: 100vw;
-  //height: calc(100%);
+  height: 100vh;
   background-color: ${(props: ContainerProps) => (props.BGcolor ? props.BGcolor : '#FFFF00')};
 `;
-//  width: ${(props: SideMenuProps) => props.larguraSideMenu === null || props.larguraSideMenu === undefined ? (
-//         5
-//     ) : (
-//         props.larguraSideMenu
-//     )}%;
